bd2d2d83986592824cde4e2d76e5a0448d348f3d
Neto Ribeiro <netorribeiro.2007@gmail.com>
2024-11-05 15:00:20 -0300

    add: modificação no botão entrar caso já exista o usuário

--- Views/tela_login.Designer.cs
+++ Views/tela_login.Designer.cs
@@ -40,9 +40,9 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Showcard Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(47, 45);
+            label1.Location = new Point(41, 34);
             label1.Name = "label1";
-            label1.Size = new Size(157, 37);
+            label1.Size = new Size(123, 30);
             label1.TabIndex = 0;
             label1.Text = "Usuário:";
             // 
@@ -50,27 +50,29 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Showcard Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(47, 195);
+            label2.Location = new Point(41, 146);
             label2.Name = "label2";
-            label2.Size = new Size(121, 37);
+            label2.Size = new Size(95, 30);
             label2.TabIndex = 1;
             label2.Text = "Senha:";
             // 
             // textbox_usuário
             // 
             textbox_usuário.Font = new Font("Segoe UI", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textbox_usuário.Location = new Point(47, 85);
+            textbox_usuário.Location = new Point(41, 64);
+            textbox_usuário.Margin = new Padding(3, 2, 3, 2);
             textbox_usuário.Name = "textbox_usuário";
-            textbox_usuário.Size = new Size(681, 57);
+            textbox_usuário.Size = new Size(596, 47);
             textbox_usuário.TabIndex = 2;
             textbox_usuário.TextChanged += textbox_usuário_TextChanged_1;
             // 
             // textbox_senha
             // 
             textbox_senha.Font = new Font("Segoe UI", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textbox_senha.Location = new Point(47, 235);
+            textbox_senha.Location = new Point(41, 178);
+            textbox_senha.Margin = new Padding(3, 2, 3, 2);
             textbox_senha.Name = "textbox_senha";
-            textbox_senha.Size = new Size(681, 57);
+            textbox_senha.Size = new Size(596, 47);
             textbox_senha.TabIndex = 3;
             textbox_senha.TextChanged += textbox_senha_TextChanged;
             // 
@@ -78,20 +80,23 @@
             // 
             button_entrar.BackColor = Color.DeepSkyBlue;
             button_entrar.Font = new Font("Showcard Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button_entrar.Location = new Point(101, 333);
+            button_entrar.Location = new Point(88, 250);
+            button_entrar.Margin = new Padding(3, 2, 3, 2);
             button_entrar.Name = "button_entrar";
-            button_entrar.Size = new Size(200, 92);
+            button_entrar.Size = new Size(175, 69);
             button_entrar.TabIndex = 4;
             button_entrar.Text = "Entrar";
             button_entrar.UseVisualStyleBackColor = false;
+            button_entrar.Click += button_entrar_Click;
             // 
             // button_cadastrar
             // 
             button_cadastrar.BackColor = Color.DeepSkyBlue;
             button_cadastrar.Font = new Font("Showcard Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button_cadastrar.Location = new Point(457, 333);
+            button_cadastrar.Location = new Point(400, 250);
+            button_cadastrar.Margin = new Padding(3, 2, 3, 2);
             button_cadastrar.Name = "button_cadastrar";
-            button_cadastrar.Size = new Size(200, 92);
+            button_cadastrar.Size = new Size(175, 69);
             button_cadastrar.TabIndex = 5;
             button_cadastrar.Text = "Cadastrar";
             button_cadastrar.UseVisualStyleBackColor = false;
@@ -99,16 +104,17 @@
             // 
             // tela_login
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
             Controls.Add(button_cadastrar);
             Controls.Add(button_entrar);
             Controls.Add(textbox_senha);
             Controls.Add(textbox_usuário);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "tela_login";
             Text = "Login";
             ResumeLayout(false);
--- Views/tela_login.cs
+++ Views/tela_login.cs
@@ -1,4 +1,7 @@
 
+using Projeto_Agenda.controller;
+using Projeto_Agenda.Views;
+
 namespace Projeto_Agenda
 {
     public partial class tela_login : Form
@@ -20,7 +23,7 @@ namespace Projeto_Agenda
         }
         private void textbox_senha_TextChanged(object sender, EventArgs e)
         {
-          habilitarbotaologin();
+            habilitarbotaologin();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -37,6 +40,18 @@ namespace Projeto_Agenda
         private void textbox_usuário_TextChanged_1(object sender, EventArgs e)
         {
 
+        }
+
+        private void button_entrar_Click(object sender, EventArgs e)
+        {
+            UsuarioController controleusuario = new UsuarioController();
+            bool resultado = controleusuario.LogarUsuario(textbox_usuário.Text, textbox_senha.Text);
+            MessageBox.Show(resultado.ToString());
+            if (resultado)
+            {
+                frm_Principal principal = new frm_Principal();
+                principal.Show();
+            }
         }
     }
 }
