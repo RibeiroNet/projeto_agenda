da5e0451850edeb982c12e96d110858ad8d5783b
Neto Ribeiro <netorribeiro.2007@gmail.com>
2024-11-18 11:28:35 -0300

    add: "data" restaured

---
+++ data/conexaoDB.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Agenda.data
+{
+    static internal class conexaoDB
+    {
+        static public MySqlConnection CriarConexao()
+        {
+            string string_conexao = "Server=127.0.0.1; DataBase=dbAgenda;User ID=root;Password=root;";
+            MySqlConnection conexao = new MySqlConnection(string_conexao);
+            return conexao;
+        }
+
+
+        static public MySqlConnection CriarConexao(string usuario, string senha)
+        {
+            string string_conexao = $"Server=127.0.0.1; DataBase=dbAgenda;User ID='{usuario}';Password='{senha}';";
+            MySqlConnection conexao = new MySqlConnection(string_conexao);
+            return conexao;
+        }
+    }
+}
