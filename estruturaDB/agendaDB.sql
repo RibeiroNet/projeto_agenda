d2196c4a9e02f5018a68d6e1d852da85ae0118a2
Leticia Lemes <leticia.lemes4384@gmail.com>
2024-11-19 14:46:05 -0300

    add:

--- estruturaDB/agendaDB.sql
+++ estruturaDB/agendaDB.sql
@@ -17,10 +17,24 @@ nome_categoria varchar(20) not null
 select cod_categoria as 'Código', nome_categoria as 'Categoria'
 from tb_categoria;
 
+delimiter $$
 create trigger trInsertCategoria 
 before
 insert
 on tb_Categoria for each row 
 begin 
-   set new.usuario = current_user()
-   end
+   set new.usuario = current_user();
+   end;
+$$
+
+delimiter ;
+insert into tb_Categoria(categoria)
+values ("amigos")
+
+select * from tb_Categoria;
+
+create user 'User1'@'%' identified by '12345678';
+
+select * from tbUsuarios;
+
+select * from mysql.user;
